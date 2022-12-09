f201352bbace60d04cb13b176444707ae28723fd
Varad Kelkar <93907658+varad0207@users.noreply.github.com>
2022-12-09 21:53:56 +0530

    Update tanker.php

--- controllers/tanker.php
+++ controllers/tanker.php
@@ -1,7 +1,7 @@
 <?php
 require "includes/common.php";
-if (!isset($_SESSION['emp_id']))
-    header("location:../html/login.html");
+// if (!isset($_SESSION['emp_id']))
+//     header("location:../views/login.html");
 if (isset($_POST['submit']) && !empty($_POST['submit'])) {
     $emp_id = $_SESSION['emp_id'];
     $get_accomodation_id = "select acc_id from security where emp_id='{$emp_id}'";
@@ -12,7 +12,12 @@ if (isset($_POST['submit']) && !empty($_POST['submit'])) {
     $submit1 = mysqli_query($conn, $get_accomodation_location) or die(mysqli_error($conn));
     $row1 = mysqli_fetch_array($submit1);
     $location = $row1['acc_name'];
-    $insert = "insert into tankers(acc_id,security_emp_id,tanker_id,location,quality_check,qty, bill_no,vendor_name) values ('$acc_id','$emp_id','tanker_id','$location','quality_check','qty', 'bill_no','vendor_name')";
+    $vname = $row1['vname'];
+    $billno = $row1['billno'];
+    $quality = $row1['quality'];
+    $qty = $row1['qty'];
+    $timestamp = time();
+    $insert = "insert into tankers(id,acc_id,security_emp_id,location,quality_check,qty,bill_no,vendor_name,timestamp) values ('','$acc_id','$emp_id','$location','$quality','$qty', '$billno','$vname','$timestamp')";
     $submit = mysqli_query($conn, $insert) or die(mysqli_error($conn));
-    header("loaction:../views/tankers.html");
+    header("loaction: ../index.html");
 }

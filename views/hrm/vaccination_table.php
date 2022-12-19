5657b726dd40b747c4dad8de9d02e4fa806d83ef
Shubham5206 <shubhamtendulkar40821@gmail.com>
2022-12-19 17:15:33 +0530

    Display tables, redone

--- views/hrm/vaccination_table.php
+++ views/hrm/vaccination_table.php
@@ -1,5 +1,6 @@
 <?php include('../../controllers/includes/common.php'); ?>
-<?php include('../../controllers/vaccination_controller.php'); ?>
+<?php include('../../controllers/employee_controller.php'); ?>
+
 <?php
 if (isset($_GET['edit'])) {
     $vaccination_id = $_GET['edit'];
@@ -17,28 +18,53 @@ if (isset($_GET['edit'])) {
     // }
 }
 ?>
-<!DOCTYPE html>
-<html>
+
+<!doctype html>
+<html lang="en">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Delta@STAAR | Vaccination-Entry</title>
-    <meta name="description" content="Vaccination records of the employees">
-    <link rel="stylesheet" href="../../css/forms.css">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>DELTA@STAAR | Vaccination</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" type="text/css" href="../../css/AccommodationView.css">
+   
 </head>
 
 <body>
-    <div class="container">
-        <h1 class="tc f1 lh-title">Vaccination Log</h1>
-        <div class="row mx-0 justify-content-center">
-            <div class="col-md-7 col-lg-5 px-lg-2 col-xl-4 px-xl-0 bg f4 lh-copy">
-                <?php if (isset($_SESSION['message'])): ?>
+    <div style="color:white;">Add Navbar</div>
+    <div class="" style="margin: 0% 5.1%;">
+        <div class="row">
+            <div class="col-1">
+                <!--Link back page, remmove target and rel if you dont want it to open the link in a new tab-->
+                <a role="button" href="#" target="_blank" rel="noopener noreferrer">
+                    <i class="bi bi-arrow-left-circle" style="font-size: 2rem; color: white;"></i>
+                </a>
+            </div>
+            <div class="col-9">
+                <h1 class="text-center">Vaccination</h1>
+            </div>
+            <div class="col ml-5 sort">
+                <a class="button" role="button" href="#">
+                    <i class="bi bi-sort-down-alt" style="font-size: 1.5rem; color: white;">Sort by</i>
+                </a>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="table-div">
+
+        <div class="table-responsive bg-white">
+        <?php if (isset($_SESSION['message'])): ?>
                 <div class="msg">
                     <?php
                     echo $_SESSION['message'];
@@ -46,18 +72,19 @@ if (isset($_GET['edit'])) {
                     ?>
                 </div>
                 <?php endif ?>
+
                 <?php $results = mysqli_query($conn, "SELECT * FROM vaccination"); ?>
-            </div>
-            <table class="table table-bordered">
-                <thead class="thead-dark">
+
+            <table class="table table-hover m-0">
+                <thead style="border: 2px solid black;">
                     <tr>
                         <th scope="col">Employee Code</th>
-                        <th scope="col">Category </th>
-                        <th scope="col">Date of Administration </th>
-                        <th scope="col">Location </th>
-                        <th scope="col">Date of next dose </th>
-                        <th scope="col" colspan="2" align="center">Action</th>
-                    </tr>
+                            <th scope="col">Category </th>
+                            <th scope="col">Date of Administration </th>
+                            <th scope="col">Location </th>
+                            <th scope="col">Date of next dose </th>
+                            <th scope="col" colspan="2" align="center">Action</th>
+                        <tr>
                 </thead>
                 <tbody>
                     <?php while ($row = mysqli_fetch_array($results)) { ?>
@@ -86,16 +113,50 @@ if (isset($_GET['edit'])) {
                             <?php echo $row['date_of_next_dose']; ?>
                         </td>
                         <td><a href="./vaccination.php?edit=<?php echo '%27' ?><?php echo $row['vaccination_id']; ?><?php echo '%27' ?>"
-                                class="edit_btn"><button type="button" class="btn btn-success">Edit</button></a></td>
-                        <td><a href="../../controllers/vaccination_controller.php?del=<?php echo '%27' ?><?php echo $row['vaccination_id']; ?><?php echo '%27' ?>"
-                                class="del_btn"><button type="button" class="btn btn-danger">Delete</button></a></td>
+                                class="edit_btn"><i class="bi bi-pencil-square" style="font-size: 1.2rem; color: black;"></i></a>
+                                &nbsp;
+                        <a href="../../controllers/vaccination_controller.php?del=<?php echo '%27' ?><?php echo $row['vaccination_id']; ?><?php echo '%27' ?>"
+                                class="del_btn"><i class="bi bi-trash" style="font-size: 1.2rem; color: black;"></a></td>
                     </tr>
                     <?php } ?>
                 </tbody>
             </table>
         </div>
+
+
     </div>
+    <div class="" style="margin:2% 5%;">
+        <div class="row">
+            <div class="col-2">
+                <!--Link back page, remmove target and rel if you dont want it to open the link in a new tab-->
+                <a role="button" href="#" target="_blank" rel="noopener noreferrer">
+                    <i class="bi bi-file-earmark-pdf" style="font-size: 1.5rem; color: white;">Export</i>
+                </a>
+            </div>
+            <div class="col-8">
+
+            </div>
+            <div class="col-2">
+
+                <a role="button" class="btn btn-light" href="../../form templates//emp.html">
+                    Add Employee
+                </a>
+
+            </div>
+        </div>
     </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>

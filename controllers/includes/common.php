b5492966ffe20097eec87ba86ed11a1f175854f4
Chinmay Joshi <chinmayujoshi@gmail.com>
2022-12-21 08:17:11 +0530

    checking rights on dashboard (#45)
    * login session workiing
    
    * logtout path issue
    
    * login_history working
    
    * superadmin or not
    
    * rename supradmin to dashboard

--- controllers/includes/common.php
+++ controllers/includes/common.php
@@ -1,6 +1,6 @@
 <?php
     // connection variable ->mysqli_connect(host,username,password,dbname);
-    $conn=mysqli_connect("localhost","root","","deltaSTAAR1_1")or die(mysqli_error($conn));
+    $conn=mysqli_connect("localhost","root","","deltaSTAAR1.1")or die(mysqli_error($conn));
     if(!isset($_SESSION['emp_id'])){
         session_start();
     } 

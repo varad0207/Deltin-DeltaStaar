99177eeec4daabbf27762969dd4326aac1a00575
varad0207 <93907658+varad0207@users.noreply.github.com>
2022-12-30 15:44:13 +0530

    vaccination category

--- controllers/vaccination_category_controller.php
+++ controllers/vaccination_category_controller.php
@@ -11,11 +11,11 @@
         session_start(); 
     } 
 
-	$category_name = "";
-    
-	$update = false;
+	$category_name = $category_id = "";
 
-	if (isset($_POST['submit'])) {
+
+	if (isset($_POST['submit'])) 
+    {
 	$category_name =  $_POST['category_name'];
     
 		mysqli_query($conn, "INSERT INTO vaccination_category( category_name, category_id) values ( '$category_name', '')"); 
@@ -23,17 +23,18 @@
 		header('location: ../views/config/vaccination_category_table.php');
 	}
 
-    if (isset($_POST['update'])) {
+    if (isset($_POST['update'])) 
+    {
+        $category_id = $_POST['category_id'];
         $category_name =  $_POST['category_name'];
         
-    
-        mysqli_query($conn, "UPDATE vaccination_category SET category_name='$category_name', WHERE category_id=$category_id");
+        mysqli_query($conn, "UPDATE vaccination_category SET category_name = '$category_name' WHERE category_id = '$category_id'");
         $_SESSION['message'] = "Vaccination Category Info updated!"; 
         header('location: ../views/config/vaccination_category_table.php');
     }
 
     if (isset($_GET['del'])) {
-        $emp_code = $_GET['del'];
+        $category_id = $_GET['del'];
         mysqli_query($conn, "DELETE FROM vaccination_category WHERE category_id=$category_id");
         $_SESSION['message'] = "Vaccination Category deleted!"; 
         header('location: ../views/config/vaccination_category_table.php');

7b8cecb9185a976df187b06d07e748a4ec643fc8
Chinmay Joshi <chinmayujoshi@gmail.com>
2023-02-13 00:23:38 +0530

    change password

--- controllers/includes/common.php
+++ controllers/includes/common.php
@@ -1,6 +1,6 @@
 <?php
     // connection variable ->mysqli_connect(host,username,password,dbname);
-    $conn=mysqli_connect("localhost","root","","deltastaar")or die(mysqli_error($conn));
+    $conn=mysqli_connect("localhost","root","root","deltastaar")or die(mysqli_error($conn));
     if(!isset($_SESSION['emp_id'])){
         session_start();
     } 

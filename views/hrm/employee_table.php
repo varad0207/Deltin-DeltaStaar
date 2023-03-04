cbc397d947eb9721f2a393f52783cbde12fbb27f
Chinmay Joshi <chinmayujoshi@gmail.com>
2023-03-04 23:06:15 +0530

    add deletion popup to vaacination

--- views/hrm/employee_table.php
+++ views/hrm/employee_table.php
@@ -20,8 +20,10 @@ if ($rights['rights_employee_details'] > 0) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../../css/overlay.css">
+    
     <!--Favicon link-->
     <link rel="icon" type="image/x-icon" href="../../images/logo-no-name-circle.png">
     <title>DELTA@STAAR | Employee Details</title>

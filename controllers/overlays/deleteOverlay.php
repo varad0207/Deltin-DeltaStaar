2402b76e898055106689f619b742d3bd23981879
Chinmay Joshi <chinmayujoshi@gmail.com>
2023-03-04 20:24:05 +0530

    delete popup

---
+++ controllers/overlays/deleteOverlay.php
@@ -0,0 +1,29 @@
+<?php require "../../controllers/includes/common.php";
+ ?>
+
+    <!-- overlay code start -->
+    <div class="overlay" id="overlay"> 
+        <div class="overlay-window">
+            <div class="overlay-window-titlebar">
+                <span class="overlay-title">Confirm Delete</span>
+                <button class="close material-icons" onclick="document.getElementById('overlay').style.display='none'">close</button>
+            </div>
+            <div class="overlay-content" style="color:black;text-align:center">
+                This action cannot be un-done<br><br>
+                    <div class="table-footer pa4">
+                        <form target="_self" method="get">
+                            <div class="fl w-75 tl">
+                                <Button class="btn btn-warning" value="true" name="delete" onclick="document.getElementById('overlay').style.display='none';location.reload();">Yes,Delete</Button>
+                            </div>
+                        </form>
+                        <div class="fl w-25 tr">
+                            <Button class="btn btn-secondary" onclick="document.getElementById('overlay').style.display='none'">No,Cancel</Button>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <!-- overlay end -->
+
+    <!-- action="../../controllers/employee_controller.php?del= + <?php //echo $row['emp_code']; ?>" -->
+

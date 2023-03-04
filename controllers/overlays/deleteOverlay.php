6410edba1e384ff90ac7afa8c569a2868cf01cb1
Chinmay Joshi <chinmayujoshi@gmail.com>
2023-03-04 22:55:44 +0530

    add responsiveness to delete popup

--- controllers/overlays/deleteOverlay.php
+++ controllers/overlays/deleteOverlay.php
@@ -5,20 +5,14 @@
     <div class="overlay" id="overlay"> 
         <div class="overlay-window">
             <div class="overlay-window-titlebar">
-                <span class="overlay-title">Confirm Delete</span>
+                <span class="overlay-title">Confirm <span class="name">Delete</span></span>
                 <button class="close material-icons" onclick="document.getElementById('overlay').style.display='none'">close</button>
             </div>
             <div class="overlay-content" style="color:black;text-align:center">
                 This action cannot be un-done<br><br>
-                    <div class="table-footer pa4">
-                        <!-- <form> -->
-                            <div class="fl w-75 tl">
-                                <Button class="btn btn-warning"  onclick="document.getElementById('del_response').submit();document.getElementById('overlay').style.display='none'">Yes,Delete</Button>
-                            </div>
-                        <!-- </form> -->
-                        <div class="fl w-25 tr">
-                            <Button class="btn btn-secondary" onclick="document.getElementById('overlay').style.display='none'">No,Cancel</Button>
-                        </div>
+                    <div class="btns">
+                            <Button class="btn btn-warning btn1"  onclick="document.getElementById('del_response').submit();document.getElementById('overlay').style.display='none'">Yes,Delete</Button>
+                            <Button class="btn btn-secondary btn2" onclick="document.getElementById('overlay').style.display='none'">No,Cancel</Button>
                     </div>
             </div>
         </div>

7940e3bee15d0b8a64e82f4654423b36daa5eb41
Navin-Morajkar <navinsika@gmail.com>
2023-04-14 15:32:46 +0530

    security_db_link-updated

--- views/security/employee_outing.php
+++ views/security/employee_outing.php
@@ -1,13 +1,17 @@
 <?php
 include('../../controllers/includes/common.php');
 include('../../controllers/employee_outing_controller.php');
-if (!isset($_SESSION["emp_id"])) header("location:../../views/login.php");
+
+if (!isset($_SESSION["emp_id"]))
+  header("location:../../views/login.php");
 $isPrivilaged = 0;
 $rights = unserialize($_SESSION['rights']);
+
 if ($rights['rights_vaccination'] > 1) {
   $isPrivilaged = $rights['rights_vaccination'];
 } else
   die('<script>alert("You dont have access to this page, Please contact admin");window.location = history.back();</script>');
+
 if ($isPrivilaged == 5 || $isPrivilaged == 4)
   die('<script>alert("You dont have access to this page, Please contact admin");window.location = history.back();</script>');
 
@@ -38,7 +42,8 @@ if (isset($_GET['edit'])) {
   <link rel="stylesheet" href="../../css/form.css">
   <link rel="stylesheet" href="../../css/style1.css">
   <!-- Bootstrap 5 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <!-- Bootstrap Icons -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
   <!-- Tachyons -->
@@ -46,9 +51,10 @@ if (isset($_GET['edit'])) {
   <!-- CSS files -->
   <link rel="stylesheet" href="../../css/table.css">
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css" />
-  
+
 </head>
 
 <body class="b ma2">
@@ -64,13 +70,14 @@ if (isset($_GET['edit'])) {
         <div class="form-content">
           <div class="form-items">
             <h1 class="f2 lh-copy tc" style="color: white;">Employee Outing Details</h1>
-            <form class="requires-validation f3 lh-copy" novalidate action="../../controllers/employee_outing_controller.php" method="post">
+            <form class="requires-validation f3 lh-copy" novalidate
+              action="../../controllers/employee_outing_controller.php" method="post">
               <div class="col-md-12 pa2">
                 <label for="empcode">Employee Code</label>
                 <?php if (isset($_GET['edit'])) { ?>
                   <input type="text" name="emp_code" value=<?php echo $emp_code ?> readonly>
-                <?php
-                } else {  ?>
+                  <?php
+                } else { ?>
                   <select class="form-select mt-3" name="emp_code" id="empcode" required onchange="freq()">
                     <option selected disabled value="" name="employee_code">Select Employee Code</option>
                     <?php
@@ -97,7 +104,8 @@ if (isset($_GET['edit'])) {
                   <?php
                   $sql1 = mysqli_query($conn, "SELECT * FROM outing_type");
                   foreach ($sql1 as $row1) { ?>
-                    <option name="outing_type" value="<?= $row1["type_id"] ?>"><?= $row1["type_name"]; ?></option> <?php } ?>
+                    <option name="outing_type" value="<?= $row1["type_id"] ?>"><?= $row1["type_name"]; ?></option>
+                  <?php } ?>
                 </select>
                 <div class="invalid-feedback">Please select an option!</div>
               </div>
@@ -108,9 +116,11 @@ if (isset($_GET['edit'])) {
               <div class="form-button mt-3 tc">
                 <div class="form-button mt-3 tc">
                   <?php if (!isset($_GET['edit'])) { ?>
-                    <button id="submit" name="submit" value="sumbit" type="submit" class="btn btn-warning f3 lh-copy" style="color: white;">Submit</button>
+                    <button id="submit" name="submit" value="sumbit" type="submit" class="btn btn-warning f3 lh-copy"
+                      style="color: white;">Submit</button>
                   <?php } else { ?>
-                    <button id="submit" name="update" value="update" type="submit" class="btn btn-warning f3 lh-copy" style="color: white;">Update</button>
+                    <button id="submit" name="update" value="update" type="submit" class="btn btn-warning f3 lh-copy"
+                      style="color: white;">Update</button>
                   <?php } ?>
                 </div>
               </div>
@@ -128,20 +138,26 @@ if (isset($_GET['edit'])) {
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
   <script src="../../js/emp.js"></script>
   <script>
     // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
+    (function () {
       'use strict';
-      window.addEventListener('load', function() {
+      window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-          form.addEventListener('submit', function(event) {
+        var validation = Array.prototype.filter.call(forms, function (form) {
+          form.addEventListener('submit', function (event) {
             if (form.checkValidity() === false) {
               event.preventDefault();
               event.stopPropagation();
@@ -152,15 +168,15 @@ if (isset($_GET['edit'])) {
       }, false);
     })();
 
-    document.querySelector("#empcode").onclick = (event)=>{
+    document.querySelector("#empcode").onclick = (event) => {
       var selectedValue = document.getElementById("empcode").value;
 
-   if(selectedValue){
-    freq();
+      if (selectedValue) {
+        freq();
 
-   }
-   
-}
+      }
+
+    }
 
     function freq() {
       var selectedValue = document.getElementById("empcode").value;
@@ -169,9 +185,9 @@ if (isset($_GET['edit'])) {
         if (this.readyState == 4 && this.status == 200) {
           var myObj = this.responseText;
           console.log(myObj)
-          if(myObj){
+          if (myObj) {
             const freq_div = document.querySelector('#outing-freq');
-            freq_div.innerHTML=myObj;
+            freq_div.innerHTML = myObj;
             document.getElementById('outing-freq').style.display = 'flex';
           }
         }

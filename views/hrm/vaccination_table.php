2542ec248eada88a31a66f456838b4b2cfaadba7
varad0207 <93907658+varad0207@users.noreply.github.com>
2023-04-20 23:03:00 +0530

    pagination for vaccination

--- views/hrm/vaccination_table.php
+++ views/hrm/vaccination_table.php
@@ -190,8 +190,24 @@ die('<script>alert("You dont have access to this page, Please contact admin");wi
     <?php
     /* ***************** PAGINATION ***************** */
     $limit=10;
+    $pages = 0;
     $page=isset($_GET['page'])?$_GET['page']:1;
-    $start=($page-1) * $limit;
+    //check if current page is less then or equal 1
+    if(($page>=1)||($page<$pages))
+    {
+        $start=($page-1) * $limit;
+        $Previous=$page-1;
+        $Next=$page+1;
+    }
+    if($page<1)
+    {
+        $Previous=1;
+        $start = 1;
+    }
+    if($page>=$pages)
+    {
+        $Next=$pages;
+    }
     $sql .=" LIMIT $start,$limit";
     $result=mysqli_query($conn,$sql);
 
@@ -199,20 +215,7 @@ die('<script>alert("You dont have access to this page, Please contact admin");wi
     $result1=mysqli_query($conn,$q1);
     $total=mysqli_num_rows($result1);
     $pages=ceil($total/$limit);
-    //check if current page is less then or equal 1
-    if(($page>1)||($page<$pages))
-    {
-        $Previous=$page-1;
-        $Next=$page+1;
-    }
-    if($page<=1)
-    {
-        $Previous=1;
-    }
-    if($page>=$pages)
-    {
-        $Next=$pages;
-    }
+    
     /* ************************************************ */
     ?>
     <div class="table-div">

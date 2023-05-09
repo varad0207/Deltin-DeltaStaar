d5b8ef70214e7fe64caec7a03c8f3fe759eb5f1c
varad0207 <93907658+varad0207@users.noreply.github.com>
2023-05-09 22:18:22 +0530

    update

--- views/accomodation/rooms.php
+++ views/accomodation/rooms.php
@@ -82,20 +82,20 @@ if (isset($_GET['edit'])) {
                                     // $accrow = mysqli_fetch_assoc($acc_code);
                                     // $no_of_rooms = $accrow['no_of_rooms'];
                                     foreach ($acc_code as $row) { ?>
-                                        <option name="acc" value="<?= $row["acc_id"] ?>"
+                                        <option name="acc" value="<?= @$row["acc_id"] ?>"
                                             <?php 
-                                            $acc_id = $row['acc_id'];
+                                            $acc_id = @$row['acc_id'];
                                             $sql2 = mysqli_query($conn, "SELECT COUNT(id) AS count_room FROM `rooms` WHERE acc_id='$acc_id' GROUP BY `acc_id`");
                                             $roomCount = mysqli_fetch_array($sql2);
-                                            $room_count = $roomCount['count_room'];
-                                            if($acc_id == $row['acc_id']) { 
-                                                if($room_count < $row['no_of_rooms']) { ?> 
+                                            $room_count = @$roomCount['count_room'];
+                                            if(@$acc_id == @$row['acc_id']) { 
+                                                if(@$room_count < @$row['no_of_rooms']) { ?> 
                                                  
                                             <?php } 
                                             else { ?>
                                                 disabled
                                             <?php } ?>>
-                                            <?= $row['acc_name']; ?>
+                                            <?= @$row['acc_name']; ?>
                                             <?php } ?>
                                         </option>
                                     <?php } ?>

a1503dcd59ef48e4f2f198e1a3aaf7927350b946
Navin-Morajkar <navinsika@gmail.com>
2023-05-30 01:01:11 +0530

    Line chart working

--- charts/waterTankers_Line.php
+++ charts/waterTankers_Line.php
@@ -10,6 +10,41 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
+    //Dummy data, Write SQL queries here
+    const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+    const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+    new Chart("myLineChart", {
+        type: "line",
+        data: {
+            //and pass here the labels on X axis
+            labels: xValues,
+            
+            datasets: [{
+                label: "Us Bhay Us",    
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(255,255,255,1.0)",
+                
+                //pass variable for data to be plotted
+                data: yValues
+            }]
+        },
+        options: {
+            //aspectRatio: 1.2,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+</script>
+
+<!-- Old Script Code -->
+<!-- <script>
     //Setup Block
     const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     const data = {
@@ -41,4 +76,4 @@
     const myLineChart = new Chart(
         document.getElementById('myLineChart'), configLine
     );
-</script>
+</script> -->

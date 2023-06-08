d2160b3c3edd6321014e745134b803ef12d0cad4
VadirajInamdar <vadirajinamdar6@gmail.com>
2023-06-08 21:50:31 +0530

    tanker_graph

--- charts/waterTankers_Line.php
+++ charts/waterTankers_Line.php
@@ -1,4 +1,4 @@
-<!-- <div class="chart-container"> -->
+<div class="chart-container">
 <!-- Ignore Comments for now, only this div below is included + scripting -->
 <!-- Refer accommodationStatus_Bar.php if needed -->
 <div class="chartBox">
@@ -9,134 +9,228 @@
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <?php include('../controllers/includes/common.php');
-try{
-$tanker_detail = "SELECT
-                  MONTH(t.timestamp) AS 'month',
-                  YEAR(t.timestamp) AS 'year',
-                  tv.id AS vendor_id,
-                  tv.vname AS 'vendor_name',
-                  COUNT(*) AS 'count_per_month'
-                  FROM
-                      tankers t
-                  JOIN
-                      tanker_vendors tv ON tv.id = t.vendor_id
-                  GROUP BY
-                      MONTH(t.timestamp),
-                      YEAR(t.timestamp),
-                      tv.id,
-                      tv.vname
-                  ORDER BY
-                      YEAR(t.timestamp),
-                      MONTH(t.timestamp),
-                      tv.id;
-                  ";
-                   $result=mysqli_query($conn,$tanker_detail);
-                   
-                   $res=array();
-                   while ($row = $result->fetch_assoc()) 
-                   {
-                       $entry = array(
-                           'month' => $row['month'],
-                           'year' => $row['year'],
-                           'vendor_id' => $row['vendor_id'],
-                           'vendor_name' => $row['vendor_name'],
-                           'count_per_month' => $row['count_per_month']
-                       );
-                       $res[]=$entry;
-                   }
-                   
-                       //Different arrays storing data of tankers of respective month.
-                       $jan = array();
-                       $feb = array();
-                       $mar = array();
-                       $apr = array();
-                       $may = array();
-                       $jun = array();
-                       $jul = array();
-                       $aug = array();
-                       $sep = array();
-                       $oct = array();
-                       $nov = array();
-                       $dec = array();
+// try {
+//     $tanker_detail = "SELECT
+//     MONTH(t.timestamp) AS 'month',
+//     YEAR(t.timestamp) AS 'year',
+//     tv.id AS vendor_id,
+//     tv.vname AS 'vendor_name',
+//     COUNT(*) AS 'count_per_month'
+//     FROM
+//         tankers t
+//     JOIN
+//         tanker_vendors tv ON tv.id = t.vendor_id
+//     GROUP BY
+//         MONTH(t.timestamp),
+//         YEAR(t.timestamp),
+//         tv.id,
+//         tv.vname
+//     ORDER BY
+//         YEAR(t.timestamp),
+//         MONTH(t.timestamp),
+//         tv.id";
+// $result = mysqli_query($conn, $tanker_detail);
 
-                        for ($i = 0; $i < 12; $i++) {
-                            foreach ($res as $entry) {
-                                if ($i + 1 == $entry['month']) {
-                                    switch ($i + 1) {
-                                        case 1:
-                                            $jan[] = $entry;
-                                            break;
-                                        case 2:
-                                            $feb[] = $entry;
-                                            break;
-                                        case 3:
-                                            $mar[] = $entry;
-                                            break;
-                                        case 4:
-                                            $apr[] = $entry;
-                                            break;
-                                        case 5:
-                                            $may[] = $entry;
-                                            break;
-                                        case 6:
-                                            $jun[] = $entry;
-                                            break;
-                                        case 7:
-                                            $jul[] = $entry;
-                                            break;
-                                        case 8:
-                                            $aug[] = $entry;
-                                            break;
-                                        case 9:
-                                            $sep[] = $entry;
-                                            break;
-                                        case 10:
-                                            $oct[] = $entry;
-                                            break;
-                                        case 11:
-                                            $nov[] = $entry;
-                                            break;
-                                        case 12:
-                                            $dec[] = $entry;
-                                            break;
-                                    }
-                                }
-                            }
-                        }
+// $months = array();
+// for ($i = 1; $i <= 12; $i++) {
+//     $months[$i] = array();
+// }
 
-}
-catch(PDOException $e) {
-    die("ERROR: Could not able to execute. " . $e->getMessage());
-}
+// while ($row = mysqli_fetch_assoc($result)) {
+//     $entry = array(
+//         'month' => $row['month'],
+//         'year' => $row['year'],
+//         'vendor_id' => $row['vendor_id'],
+//         'vendor_name' => $row['vendor_name'],
+//         'count_per_month' => $row['count_per_month']
+//     );
 
+//     $month = $entry['month'];
+//     $months[$month][] = $entry;
+// }
+
+// foreach ($months as $month => $data) {
+//     echo $month . ': ' . count($data) . '<br>';
+// }
+// $tanker_detail = "SELECT
+//     MONTH(t.timestamp) AS 'month',
+//     YEAR(t.timestamp) AS 'year',
+//     tv.id AS vendor_id,
+//     tv.vname AS 'vendor_name',
+//     COUNT(*) AS 'count_per_month'
+//     FROM
+//         tankers t
+//     JOIN
+//         tanker_vendors tv ON tv.id = t.vendor_id
+//     GROUP BY
+//         MONTH(t.timestamp),
+//         YEAR(t.timestamp),
+//         tv.id,
+//         tv.vname
+//     ORDER BY
+//         YEAR(t.timestamp),
+//         MONTH(t.timestamp),
+//         tv.id";
+// $result = mysqli_query($conn, $tanker_detail);
+
+// $months = array();
+// for ($i = 1; $i <= 12; $i++) {
+//     $months[$i] = 0;
+// }
+
+// while ($row = mysqli_fetch_assoc($result)){
+//     $month = $row['month'];
+//     $count = $row['count_per_month'];
+//     $months[$month] = $count;
+// }
+
+
+// //     // Use the $months array as needed for further processing or display
+// } catch (mysqli_sql_exception $e) {
+//  die("ERROR: Could not able to execute. " . $e->getMessage());
+//  }
 ?>
 
 <script>
     //Dummy data, Write SQL queries here
+//     var xValues = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+// var yValues = [
+//
+// ];
+
+// new Chart("myLineChart", {
+//     type: "bar",
+//     data: {
+//         labels: xValues,
+//         datasets: [{
+//             label: "Number of Trips",
+//             fill: false,
+//             lineTension: 0,
+//             backgroundColor: 'rgb(54, 162, 235)',
+//             borderColor: 'rgba(153, 102, 255, 0.5)',
+//             data: yValues
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
+
+
+//      function getRandomColor() {
+//         var letters = "0123456789ABCDEF";
+//         var color = "#";
+//         for (var i = 0; i < 6; i++) {
+//             color += letters[Math.floor(Math.random() * 16)];
+//         }
+//         return color;
+//     }
+
+// var xValues = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+//     var yValues = [
+//     <?php echo ($months[1]); ?>,
+//     <?php echo ($months[2]); ?>,
+//     <?php echo ($months[3]); ?>,
+//     <?php echo ($months[4]); ?>,
+//     <?php echo ($months[5]); ?>,
+//     <?php echo ($months[6]); ?>,
+//     <?php echo ($months[7]); ?>,
+//     <?php echo ($months[8]); ?>,
+//     <?php echo ($months[9]); ?>,
+//     <?php echo ($months[10]); ?>,
+//     <?php echo ($months[11]); ?>,
+//     <?php echo ($months[12]); ?>
+//     ];
+
+//     new Chart("myBarChart", {
+//         type: "bar",
+//         data: {
+//             labels: xValues,
+//             datasets: [{
+//                 label: "Number of Trips",
+//                 backgroundColor: 'rgb(54, 162, 235)',
+//                 borderColor: 'rgba(153, 102, 255, 0.5)',
+//                 data: yValues
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         }
+//     });
+var x=0;
+</script>
+
+<?php
+try {
+    $tanker_detail = "SELECT
+        MONTH(t.timestamp) AS 'month',
+        YEAR(t.timestamp) AS 'year',
+        tv.id AS vendor_id,
+        tv.vname AS 'vendor_name',
+        COUNT(*) AS 'count_per_month'
+        FROM
+            tankers t
+        JOIN
+            tanker_vendors tv ON tv.id = t.vendor_id
+        GROUP BY
+            MONTH(t.timestamp),
+            YEAR(t.timestamp),
+            tv.id,
+            tv.vname
+        ORDER BY
+            YEAR(t.timestamp),
+            MONTH(t.timestamp),
+            tv.id";
+    $result = mysqli_query($conn, $tanker_detail);
+
+    $months = array();
+    for ($i = 1; $i <= 12; $i++) {
+        $months[$i] = 0;
+    }
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $month = $row['month'];
+        $count = $row['count_per_month'];
+        $months[$month] = $count;
+    }
+} catch (mysqli_sql_exception $e) {
+    die("ERROR: Could not able to execute. " . $e->getMessage());
+}
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<canvas id="myBarChart"></canvas>
+<script>
     var xValues = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
     var yValues = [
-        <?php echo count($jan); ?>,
-        <?php echo count($feb); ?>,
-        <?php echo count($mar); ?>,
-        <?php echo count($apr); ?>,
-        <?php echo count($may); ?>,
-        <?php echo count($jun); ?>,
-        <?php echo count($jul); ?>,
-        <?php echo count($aug); ?>,
-        <?php echo count($sep); ?>,
-        <?php echo count($oct); ?>,
-        <?php echo count($nov); ?>,
-        <?php echo count($dec); ?>
+        <?php echo $months[1]; ?>,
+        <?php echo $months[2]; ?>,
+        <?php echo $months[3]; ?>,
+        <?php echo $months[4]; ?>,
+        <?php echo $months[5]; ?>,
+        <?php echo $months[6]; ?>,
+        <?php echo $months[7]; ?>,
+        <?php echo $months[8]; ?>,
+        <?php echo $months[9]; ?>,
+        <?php echo $months[10]; ?>,
+        <?php echo $months[11]; ?>,
+        <?php echo $months[12]; ?>
     ];
-    console.log(yValues);
-    new Chart("myLineChart", {
+
+    new Chart("myBarChart", {
         type: "bar",
         data: {
             labels: xValues,
             datasets: [{
                 label: "Number of Trips",
-                fill: false,
-                lineTension: 0,
                 backgroundColor: 'rgb(54, 162, 235)',
                 borderColor: 'rgba(153, 102, 255, 0.5)',
                 data: yValues
@@ -150,13 +244,4 @@ catch(PDOException $e) {
             }
         }
     });
-
-     function getRandomColor() {
-        var letters = "0123456789ABCDEF";
-        var color = "#";
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
 </script>

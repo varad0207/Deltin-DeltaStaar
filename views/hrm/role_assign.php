48cd9c45ef10be4169aed4b13cf5dba30c0e0670
varad0207 <93907658+varad0207@users.noreply.github.com>
2023-08-12 12:05:29 +0530

    update

--- views/hrm/role_assign.php
+++ views/hrm/role_assign.php
@@ -1,31 +1,31 @@
-<?php include('../../controllers/includes/common.php'); ?>
-<?php include('../../controllers/role_controller.php'); ?>
-<?php
+    <?php include('../../controllers/includes/common.php'); ?>
+    <?php include('../../controllers/role_controller.php'); ?>
+    <?php
 
-if (!isset($_SESSION["emp_id"]))
-header("location:../../index.php");
-if ($_SESSION['is_superadmin'] == 0)
+    if (!isset($_SESSION["emp_id"]))
+    header("location:../../index.php");
+    if ($_SESSION['is_superadmin'] == 0)
     die('<script>alert("You dont have access to this page, Please contact admin");window.location = history.back();</script>');
 
-// if (isset($_GET['edit'])) {
-//     $role_id = $_GET['edit'];
-//     $update = true;
-//     $record = mysqli_query($conn, "SELECT * FROM roles WHERE role_id=$role_id");
+    // if (isset($_GET['edit'])) {
+    //     $role_id = $_GET['edit'];
+    //     $update = true;
+    //     $record = mysqli_query($conn, "SELECT * FROM roles WHERE role_id=$role_id");
 
-//     // if (count($record) == 1 ) {
-//     $n = mysqli_fetch_array($record);
+    //     // if (count($record) == 1 ) {
+    //     $n = mysqli_fetch_array($record);
 
-//     $role_id = $n['role_id'];
-//     $role_name = $n['role_name'];
-//     $rights = $n['rights'];
-//     // }
-// }
-?>
+    //     $role_id = $n['role_id'];
+    //     $role_name = $n['role_name'];
+    //     $rights = $n['rights'];
+    //     // }
+    // }
+    ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
+    <head>
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -44,104 +44,80 @@ if ($_SESSION['is_superadmin'] == 0)
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css" />
-</head>
+    </head>
 
-<body class="b ma2">
+    <body class="b ma2">
     <!-- Sidebar and Navbar-->
-   <?php
+    <?php
     include '../../controllers/includes/sidebar.php';
     include '../../controllers/includes/navbar.php';
     ?>
-    
+
     <div class="form-body">
         <div class="row">
             <div class="form-holder">
-                <!-- <div class="form-content"> -->
-                <div class="form-items">
-                    <h1 class="f2 lh-copy tc" style="color: white;">Assign Role</h1>
-                    <form class=" f3 lh-copy" novalidate action="../../controllers/role_assign_controller.php" method="post">
-                        <?php $results = mysqli_query($conn, "SELECT * FROM employee JOIN employee_designation ON employee_designation.id = employee.designation and employee.role is null"); ?>
+                <div class="form-content">
+                    <div class="form-items">
+                        <h1 class="f2 lh-copy tc" style="color: white;">Assign Role</h1>
+                        <form class=" f3 lh-copy" novalidate action="../../controllers/role_assign_controller.php" method="post">
+                        <?php $results = mysqli_query($conn, "SELECT * FROM employee JOIN employee_designation ON employee_designation.id = employee.designation and employee.role is null"); 
+                        $req_id = '';
+                        ?>
                         <div class="pa1 table-responsive">
-                            <table class="table table-bordered tc">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Employee</th>
-                                        
-                                        <th scope="col" colspan="2">Role</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
+                            <div class="col-md-12 pa2">
+                                <select id="emp-code-dropdown" name="emp_code" oninput="">
+                                    <option name="emp_code" selected disabled value="">Select employee </option>
+                                    <?php $employees = mysqli_query($conn, "SELECT * FROM employee where role is null");
+                                    foreach ($employees as $row) {
+                                        $name = $row['fname'] . " " . $row['lname'];
+                                        ?>
+                                        <option name="emp_code" value="<?= $row["emp_id"] ?>" title="<?= $name ?>">
+                                            <?php echo("".$name." - ".$row["emp_code"]); ?>
+                                        </option>
+                                    <?php } ?>
+                                </select>
+                                <div class="valid-feedback">field is valid!</div>
+                                <div class="invalid-feedback">field cannot be blank!</div>
+                            </div>
 
-                                    <tr>
-                                        <td>
-                                            <?php
-                                            $req_id = '';
-                                            ?>
-                                            <select id="emp-code-dropdown" name="emp_code" oninput="">
+							<input type="hidden" name="acc_id" value="<?php echo $acc_id; ?>">
+                            <div class="col-md-12 pa2">
+								<select name="role_id">
+                                    <option name="role_id" selected disabled value="">Assign Role </option>
+                                    <?php
+                                    $roles = mysqli_query($conn, "SELECT * FROM roles");
 
-                                                <option name="emp_code" selected disabled value="">Select employee </option>
-                                                <?php $employees = mysqli_query($conn, "SELECT * FROM employee where role is null");
-                                                foreach ($employees as $row) {
-                                                    $name = $row['fname'] . " " . $row['lname'];
-                                                    ?>
-                                                    <option name="emp_code" value="<?= $row["emp_id"] ?>" title="<?= $name ?>">
-                                                        <?php echo("".$name." - ".$row["emp_code"]); ?>
-                                                    </option>
-                                                <?php } ?>
+                                    foreach ($roles as $row) {
+                                        // $sql = mysqli_query($conn, "SELECT rights.* FROM rights join roles on rights.id=roles.rights and roles.role_id='{$row['role_id']}'");
+                                        // $rights = mysqli_fetch_array($sql);
 
+                                    ?>
+                                        <option name="role_id" value="<?= $row["role_id"] ?>" title="">
+                                            <?= $row["role_name"]; ?>
+                                        </option>
+                                    <?php
+                                    }
 
-                                                <!-- <script type='text/javascript'>
-                                                    const myFunction = () => {
-                                                        var optionValue = document.getElementById("emp-code-dropdown").value;
-                                                       
-                                                        document.getElementById("name_emp").innerHTML = "empName";
+                                    ?>
+                                </select>
+                                <div class="valid-feedback">field is valid!</div>
+                                <div class="invalid-feedback">field cannot be blank!</div>
+                            </div>
+                            
 
-                                                    }
-                                                </script> -->
-                                            </select>
-                                        </td>
-                                       
-                                        
-                                        <td>
-                                            <select name="role_id">
-                                                <option name="role_id" selected disabled value="">Assign Role </option>
-                                                <?php
-                                                $roles = mysqli_query($conn, "SELECT * FROM roles");
-
-                                                foreach ($roles as $row) {
-                                                    // $sql = mysqli_query($conn, "SELECT rights.* FROM rights join roles on rights.id=roles.rights and roles.role_id='{$row['role_id']}'");
-                                                    // $rights = mysqli_fetch_array($sql);
-
-
-
-                                                ?>
-                                                    <option name="role_id" value="<?= $row["role_id"] ?>" title="">
-                                                        <?= $row["role_name"]; ?>
-                                                    </option>
-                                                <?php
-                                                }
-
-                                                ?>
-                                            </select>
-                                        </td>
-                                    </tr>
-
-                            </table>
-                        </div>
-                        <div class="form-button mt-3 tc">
-                            <?php if ($update == true) : ?>
-                                <button id="submit" name="update" value="update" type="submit" class="btn btn-warning f3 lh-copy" style="color: white;">Update</button>
-                            <?php else : ?>
-                                <button id="submit" name="submit" value="sumbit" type="submit" class="btn btn-warning f3 lh-copy" style="color: white;">Submit</button>
-                            <?php endif ?>
-
-                        </div>
-                    </form>
-                    <button class="btn btn-warning f3 lh-copy" style="color: white; margin-top:10px;" onclick="location.href='roles.php'">
-                        Add New Role
-                    </button>
+                            <div class="form-button mt-3 tc">
+                                <?php if ($update == true): ?>
+                                <button id="submit" class="btn btn-warning f3 lh-copy" style="color: white;"
+                                    type="submit" name="update" value="update"
+                                    style="background: #556B2F;">update</button>
+                                <?php else: ?>
+                                <button id="submit" class="btn btn-warning f3 lh-copy" style="color: white;"
+                                    type="submit" name="submit" value="submit">Submit</button>
+                                <?php endif ?>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <!-- </div> -->
             </div>
         </div>
     </div>
@@ -151,6 +127,6 @@ if ($_SESSION['is_superadmin'] == 0)
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-</body>
+    </body>
 
-</html>
+    </html>
